d6e1bc42d5bdfda7d9d33e1850cdd4b840e4d2a7
GithubKangMin <leelucky1@naver.com>
2024-10-08 17:03:38 +0900

    initial commit

---
+++ src/main/java/family/family_communication_board/FamilyCommunicationBoardApplication.java
@@ -0,0 +1,13 @@
+package family.family_communication_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCommunicationBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyCommunicationBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/family/family_communication_board/FamilyCommunicationBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package family.family_communication_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCommunicationBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
